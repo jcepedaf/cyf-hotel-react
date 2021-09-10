459ee1413a123b44469f913fefde3789e928c327
Jesus Cepeda <jcepedaf@gmail.com>
2021-09-10 11:27:26 +0200

    agrego homework 2 de lesson 1

---
+++ src/Heading.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export function Heading() {
+  return (
+    <header className="App-header">
+      <img
+        className="App-logo"
+        src="https://image.flaticon.com/icons/svg/139/139899.svg"
+        alt="Logo"
+      />
+      CYF Hotel
+    </header>
+  );
+}
+
+export default Heading;
