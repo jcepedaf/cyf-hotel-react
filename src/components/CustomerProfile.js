f41e82433484dc0a87a623abcce0e45d9aac936a
Jesus Cepeda <jcepedaf@gmail.com>
2021-09-23 13:20:16 +0200

    agrego homework lesson 3 punto 21

--- src/components/CustomerProfile.js
+++ src/components/CustomerProfile.js
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function CustomerProfile(props) {
+  const [customerProfile, setCustomerProfile] = useState({});
+
+  useEffect(() => {
+    fetch(`https://cyf-react.glitch.me/customers/${props.profile}`)
+      .then(res => res.json())
+      .then(customerData => {
+        setCustomerProfile(customerData);
+      });
+  }, [customerProfile]);
+
   return (
     <div>
-      <p>Customer Profile {props.profile}</p>
+      <p>Customer Profile</p>
+      <ul>
+        <li>ID: {customerProfile.id}</li>
+        <li>Email: {customerProfile.email}</li>
+        <li>VIP: {customerProfile.vip ? "YES" : "NO"}</li>
+        <li>Phone: {customerProfile.phoneNumber}</li>
+      </ul>
     </div>
   );
 }
